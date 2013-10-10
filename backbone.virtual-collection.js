d89163a6d515b7fd8ba4329be87713823589d82b
Justin Winslow <justin@jwin.me>
2013-10-09 21:30:35 -0500

    DRY

--- backbone.virtual-collection.js
+++ backbone.virtual-collection.js
@@ -32,13 +32,8 @@
      '_rebuildIndex', '_models', '_onAdd', '_onRemove', '_onChange', '_onReset',
      '_indexAdd', '_indexRemove');
 
-    if (!options.filter) {
-      this.filter = function () { return true; };
-    } else if (_.isFunction(options.filter)) {
-      this.filter = options.filter;
-    } else if (options.filter.constructor === Object) {
-      this.filter = VirtualCollection.buildFilterFromHash(options.filter);
-    }
+    // set filter
+    this.filter = VirtualCollection.buildFilter(options.filter);
 
     // build index
     this._rebuildIndex();
@@ -55,15 +50,21 @@
 
   /**
    * [static] Returns a function that returns true for models that meet the specified conditions
-   * @param  {Object} hash of model attributes
+   * @param  {Object} hash of model attributes or {Function} filter
    * @return {Function} filtering function
    */
-  VirtualCollection.buildFilterFromHash = function (hash) {
-    return function (model) {
-      return !Boolean(_(Object.keys(hash)).detect(function (key) {
-        return model.get(key) !== hash[key];
-      }));
-    };
+  VirtualCollection.buildFilter = function (filter) {
+    if (!filter) {
+      return function () { return true; };
+    } else if (_.isFunction(filter)) {
+      return filter;
+    } else if (filter.constructor === Object) {
+      return function (model) {
+        return !Boolean(_(Object.keys(filter)).detect(function (key) {
+          return model.get(key) !== filter[key];
+        }));
+      };
+    }
   };
 
   vc = VirtualCollection.prototype;
@@ -157,18 +158,12 @@
   /**
    * Change the filter and update collection
    *
-   * @param  {Object} or {Function} filter
+   * @param  {Object} hash of model attributes or {Function} filter
    * @return {VirtualCollection}
    */
 
   vc.updateFilter = function(filter){
-    if (!filter) {
-      this.filter = function () { return true; };
-    } else if (_.isFunction(filter)) {
-      this.filter = filter;
-    } else if (filter.constructor === Object) {
-      this.filter = VirtualCollection.buildFilterFromHash(filter);
-    }
+    this.filter = VirtualCollection.buildFilter(filter);
 
     this._rebuildIndex();
 
